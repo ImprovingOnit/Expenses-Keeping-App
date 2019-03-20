b0b77c0fb3bdefb42f0cb41bf94d8b351bd78132
ImprovingOnit <dngrowth01@gmail.com>
2019-03-20 16:36:55 +0700

    Adding Link and NavLink

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -36,13 +36,28 @@ const HelpPage = () => {
 
 const NotFoundPage = () => {
     return (
-        <div>404!</div>
+        <div>
+            404! - <Link to="/">Go To DashBoard</Link>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Expensify</h1>
+            <NavLink exact activeClassName="is-active" to="/">Home Page</NavLink>
+            <NavLink activeClassName="is-active" to="/create">AddExpense</NavLink>
+            <NavLink activeClassName="is-active" to="/edit">EditExpense</NavLink>
+            <NavLink activeClassName="is-active" to="/help">Help</NavLink>
+        </div>
     )
 }
 
 const routes = (
     <BrowserRouter>
         <div>
+            <Header />
             <Switch>
                 <Route path="/" exact component={ExpenseDashboardPage} />
                 <Route path="/create" component={AddExpensePage} />

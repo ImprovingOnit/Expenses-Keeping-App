21d427e22907629b48cbcc5e3412c752cce34b78
ImprovingOnit <dngrowth01@gmail.com>
2019-03-22 16:41:12 +0700

    Adding expenses reducer test

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,51 @@
+import expensesReducer from './../../reducers/expenses'
+
+const expensesReducerDefaultState = [
+    {
+        id: '123abc',
+        description: 'hello',
+        note: 'notehello',
+        amount: 0,
+        createdAt: 10000 
+    },
+    {
+        id: '123abcd',
+        description: 'hello',
+        note: 'notehello',
+        amount: 0,
+        createdAt: 10000 
+    }
+]
+
+
+test('Init Redux action', () => {
+    const state = expensesReducer(undefined, { type: '@INIT'})
+    expect(state).toEqual([])
+})
+
+test('addExpense_Reducer', () => {
+    const expense = { 
+        id: 'newversion',
+        description: 'hello',
+        note: 'notehello',
+        amount: 0,
+        createdAt: 10000 
+    }
+    const state = expensesReducer(expensesReducerDefaultState, { type: 'ADD_EXPENSE', expense })
+    expect(state).toEqual([ ...expensesReducerDefaultState, expense ])
+})
+
+test('removeExpense_Reducer', () => {
+    const id = '123abc'
+    const state = expensesReducer(expensesReducerDefaultState, { type: 'REMOVE_EXPENSE', id })
+    expect(state).toEqual([ expensesReducerDefaultState[1] ])
+})
+
+test('editExpense_Reducer', () => {
+    const id = '123abc'
+    const updates = {
+        description: 'Updated Description'
+    }
+    const state = expensesReducer(expensesReducerDefaultState, { type: 'EDIT_EXPENSE', id, updates })
+    expect(state[0].description).toBe(updates.description)
+})

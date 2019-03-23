e2c27cc77c0fd477826b450d55804cb04c9399ac
ImprovingOnit <dngrowth01@gmail.com>
2019-03-23 12:47:53 +0700

    adding config to webpack for production build mode

--- src/app.js
+++ src/app.js
@@ -7,16 +7,9 @@ import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import AppRouter from './routers/AppRouter'
 import storeExport from './store/expensesStore'
-import { addExpense } from './actions/expenses';
+
 
 const store = storeExport()
-
-store.dispatch(addExpense({ description: 'Test1', amount: 100, createdAt: 9000000 }))
-store.dispatch(addExpense({ description: 'Test2', amount: 200, createdAt: 200000}))
-store.dispatch(addExpense({ description: 'Test3', amount: 50, createdAt: 1000000000 }))
-store.dispatch(addExpense({ description: 'Test4', amount: 150, createdAt: 300000000000 }))
-
-
 
 const App = () => {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path')
 
+module.exports = (env) => {
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public/scripts'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.join(__dirname, 'public'),
-        publicPath: '/scripts/'
-    },
-    devtool: 'cheap-module-eval-source-map'
+    const isProduction = env === 'production'
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public/scripts'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        devServer: {
+            historyApiFallback: true,
+            contentBase: path.join(__dirname, 'public'),
+            publicPath: '/scripts/'
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map'
+    }    
 }
+
 
